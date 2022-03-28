82db7d54a0bf0eee13e533dbe455ca33592ce150
Farber98 <juantuc98@gmail.com>
2022-03-28 12:16:17 -0300

    feat: first program

---
+++ 02-write-your-first-program/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Print your name using Println
+func main() {
+	fmt.Println("Juan", "Juan", "Juan", "Juan", "Juan", "Juan", "Juan")
+	fmt.Print("Juan", "Juan", "Juan", "Juan", "Juan")
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module learngo-exercises
+
+go 1.17
